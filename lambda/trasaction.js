6be550ea866b59687ce74367af817d8c0e01cb92
Wu <wkimmy@88e9fe767608.ant.amazon.com>
2019-09-23 13:41:53 -0300

    add transaction code

---
+++ lambda/trasaction.js
@@ -0,0 +1,52 @@
+var AWS = require("aws-sdk");
+let resp
+
+exports.handler =  async function(event, context) {
+    var fromAmount = event.body.fromAmount - event.body.amount;
+    var toAmount = parseFloat(event.body.toAmount) + parseFloat(event.body.amount) ;
+    const params = {
+        TransactItems: [{
+            Put: {
+                TableName : '[tableName]',
+                Item: {
+                    acc: event.body.from,
+                    amount: fromAmount
+                }
+            }
+        }, {
+            Put: {
+                TableName : '[tableName]',
+                Item: {
+                    acc: event.body.to,
+                    amount: toAmount
+                }
+            }
+        }, {
+            Put: {
+                TableName : '[tableName]',
+                Item: {
+                    acc: event.body.from + event.body.to + '#' + Date.now().toString(),
+                    from: event.body.from,
+                    to: event.body.to,
+                    amount: event.body.amount
+                }
+            }
+        }]
+    };
+    
+    const documentClient = new AWS.DynamoDB.DocumentClient();
+
+    documentClient.transactWrite(params, function(err, data) {
+        if (err) {
+            console.log(err);
+            return err;
+            }
+            
+        else{
+            console.log(data);  
+            return data;
+        } 
+    });
+    console.log("EVENT: \n" + JSON.stringify('ended execution', null, 2))
+    return context.logStreamName
+}
